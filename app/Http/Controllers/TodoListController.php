4e60c2eef0e8cf6c16ec0bd0e12c8fd1cbaba172
saujanya <saujanya93@gmail.com>
2022-06-13 18:39:25 +0545

    api resource added with todolistrequest page

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoListRequest;
 use App\Models\TodoList;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,25 +15,15 @@ class TodoListController extends Controller
         return response($lists);
     }
 
-    public function test()
-    {
-        return response
-        (
-            [
-                "list" => "this is a list boys"
-            ]
-        );
-    }
-
     public function show(TodoList $todo_list)
     {
 //        $list = TodoList::findOrFail($id);
         return response($todo_list);
     }
 
-    public function store(Request $request)
+    public function store(TodoListRequest $request)
     {
-        $request->validate(['name' => ['required']]);
+//        $request->validate(['name' => ['required']]);
         $list = TodoList::create($request->all());
         return $list;
     }
@@ -43,9 +34,9 @@ class TodoListController extends Controller
         return response('',Response::HTTP_NO_CONTENT);
     }
 
-    public function update(Request $request, TodoList $todo_list)
+    public function update(TodoListRequest $request, TodoList $todo_list)
     {
-        $request->validate(['name' => 'required']);
+//        $request->validate(['name' => 'required']);
 
         $todo_list->update($request->all());
         return response($todo_list);
--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('todo-list',[TodoListController::class,'index'])->name('todo-list.index');
+Route::apiResource('todo-list',TodoListController::class);
 
-Route::get('todo-list/{todo_list}',[TodoListController::class,'show'])->name('todo-list.show');
+//Route::get('todo-list',[TodoListController::class,'index'])->name('todo-list.index');
+//
+//Route::get('todo-list/{todo_list}',[TodoListController::class,'show'])->name('todo-list.show');
 
-Route::get('test',[TodoListController::class,'test'])->name('test');
-
-Route::post('todo-list',[TodoListController::class,'store'])->name('todo-list.store');
-
-Route::delete('todo-list/{todo_list}',[TodoListController::class,'destroy'])->name('todo-list.destroy');
-
-Route::patch('todo-list/{todo_list}',[TodoListController::class,'update'])->name('todo-list.update');
+//Route::post('todo-list',[TodoListController::class,'store'])->name('todo-list.store');
+//
+//Route::delete('todo-list/{todo_list}',[TodoListController::class,'destroy'])->name('todo-list.destroy');
+//
+//Route::patch('todo-list/{todo_list}',[TodoListController::class,'update'])->name('todo-list.update');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
